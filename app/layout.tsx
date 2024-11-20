fbbd80ed5ef7526575498f08ef9985056904fb4a
Tim <poberezhny003@gmail.com>
2024-11-20 15:03:47 +0100

    Wraping layout into black theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const fontSans = Plus_Jakarta_Sans({ subsets: ["latin"],
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
-  variable: '--font-sans'
-})
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "HealthPulse",
@@ -22,9 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
